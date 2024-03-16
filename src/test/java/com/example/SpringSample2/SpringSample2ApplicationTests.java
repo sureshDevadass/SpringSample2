341cf0a035a4d4f417bd76d440989407cc107bc2
Sureshpd89 <sureshpd89@gmail.com>
2024-03-17 00:46:01 +0530

    Initial commit

---
+++ src/test/java/com/example/SpringSample2/SpringSample2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringSample2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSample2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
